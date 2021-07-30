ebf60b88e0d74db5bf9c15ae020d9945cb0d3bc9
aaris03khan <aaris03khan@gmail.com>
2021-07-30 10:22:39 +0530

    CD programs

---
+++ CD progs/lexial.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char s[20];
+	int i=0,n=1;
+	printf("Enter the string:");
+	scanf("%s",&s);
+	for(i=0;i<strlen(s);i++){
+		if((s[i] >= 'A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z')){
+			if(s[i+1]=='+'||s[i+1]=='='||s[i+1]=='-'||s[i+1]=='*'||s[i+1]=='/'||s[i+1]=='\0'){
+				printf("id %d",n);
+				n++;
+			}
+		}
+		if(s[i]>=48&&s[i]<=57){
+			printf("%c",s[i]);
+		}
+		else{
+			switch(s[i]){
+				case '+' :printf("+");
+				break;
+				case '-' :printf("-");
+				break;
+				case '*' :printf("*");
+				break;
+				case '=' :printf("=");
+				break;
+				case '/' :printf("/");
+				break;
+			}
+		}
+	}
+}
+				
+				
